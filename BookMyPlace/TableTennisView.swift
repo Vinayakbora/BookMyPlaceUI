cf2429500008919fd464edbd81db5630ff416e00
Mallika Roy <mallika.roy@bajajfinserv.in>
2024-03-24 22:54:46 +0530

    Table tennis images added

---
+++ BookMyPlace/TableTennisView.swift
@@ -0,0 +1,18 @@
+//
+//  TableTennisView.swift
+//  BookMyPlace
+//
+//  Created by Mallika Roy on 24/03/24.
+//
+
+import SwiftUI
+
+struct TableTennisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TableTennisView()
+}
